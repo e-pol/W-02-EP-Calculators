f60c3d13b743f9095800e6bbabaaed59a3a73a31
e.pol <eugene.poletski@gmail.com>
2015-09-21 19:31:45 +0300

    Create new skeleton

---
+++ javascripts/ep-calc-construct-elements.js
@@ -0,0 +1,17 @@
+;
+
+/**
+ * JavaScript module for design of beams, rafters and columns
+ *
+ * @module epCalcConstructElements
+ */
+
+var epCalcConstructElements = function () {
+    "use strict";
+    
+    var moduleId = "epCalcConstructElements";
+ }
+
+$(document).ready(epCalcConstructElements);
+
+
