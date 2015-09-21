52a2083e5d80555f16ae8e0274fc8d22e24adef5
e.pol <eugene.poletski@gmail.com>
2015-09-21 20:10:15 +0300

    Add empty pattern for root object 'elements'

--- javascripts/ep-calc-construct-elements.js
+++ javascripts/ep-calc-construct-elements.js
@@ -5,13 +5,89 @@
  *
  * @module epCalcConstructElements
  */
-
 var epCalcConstructElements = function () {
     "use strict";
-    
+
     var moduleId = "epCalcConstructElements";
- }
+
+    /**
+     * Root object for construction elements calculator
+     *
+     * @namespace epCalcConstructElements
+     * @class construction_elements_calculator
+     */
+    var elements = {
+
+      /**
+        * Print result
+        *
+        * @method print
+        * @return {Void}
+        */
+        print: function () {
+            // Prints result
+        },
+
+      /**
+        * Email result
+        *
+        * @method email
+        * @return {Void}
+        */
+        email: function () {
+            // Emails result
+        },
+
+      /**
+        * Show general calculator help
+        *
+        * @method help
+        * @return {Void}
+        */
+        help: function () {
+            // Show general help
+        },
+
+      /**
+        * Check for errors
+        *
+        * @method check
+        * @return {String} message OK or Error message
+        */
+        check: function () {
+            // Checks for errors
+        },
+
+      /**
+        * Applies changes and calls dependencies
+        *
+        * @method applyChanges
+        * @return {String} message OK or Error message
+        */
+        applyChanges: function () {
+            // Calls this.check
+            // Calls dependencies
+        },
+
+      /**
+        * Initializes root object of the module:
+        * - creates and adds to DOM new nodes;
+        * - applies event handlers;
+        * - defines dependencies;
+        *
+        * @method init
+        * @param {Object} container Parent DOM object to append new objects to
+        * @return {Void}
+        */
+        init: function (container) {
+
+        }
+    };
+
+    // init
+    var containerEl = $("#epCalcConstructElements");
+
+    elements.init(containerEl);
+};
 
 $(document).ready(epCalcConstructElements);
-
-
